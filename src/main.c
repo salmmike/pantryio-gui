3a45a0aa921a229252cf8c59771d31603b8d3f5d
Mike Salmela <mike.salmela@iki.fi>
2023-05-31 00:19:41 +0300

    Add POC communication with dbus communicator

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <glib.h>
+#include <gio/gio.h>
 
 #define G_APPLICATION_DEFAULT_FLAGS (GApplicationFlags) 0
 #define STRINT_MAX_COUNT 255
@@ -17,19 +19,86 @@ print_hello (GtkWidget *widget,
   g_print ("Hello World\n");
 }
 
+gchar*
+get_pantry_status()
+{
+    GDBusConnection *connection;
+    const gchar *response;
+    GVariant *value;
+    GError *error;
+    gchar *opt_address = "unix:abstract=pantryiocommunication";
+
+    error = NULL;
+    connection = g_dbus_connection_new_for_address_sync(opt_address,
+                                                       G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT,
+                                                       NULL, /* GDBusAuthObserver */
+                                                       NULL, /* GCancellable */
+                                                       &error);
+    if (connection == NULL) {
+          g_printerr ("Error connecting to D-Bus address %s: %s\n", opt_address, error->message);
+          g_error_free (error);
+          return EXIT_FAILURE;
+    }
+
+    value = g_dbus_connection_call_sync (connection,
+                                        NULL, /* bus_name */
+                                        "/org/gtk/GDBus/PantryIOCommunicator",
+                                        "org.pantryio.GDBus.StatusInterface",
+                                        "GetStatus",
+                                        NULL,
+                                        G_VARIANT_TYPE ("(s)"),
+                                        G_DBUS_CALL_FLAGS_NONE,
+                                        -1,
+                                        NULL,
+                                        &error);
+
+    if (value == NULL) {
+          g_printerr ("Error invoking HelloWorld(): %s\n", error->message);
+          g_error_free (error);
+        return EXIT_FAILURE;
+    }
+
+    g_variant_get(value, "(&s)", &response);
+    char* ret = calloc(strlen(response), sizeof(char));
+    strcpy(ret, response);
+
+    g_object_unref (connection);
+    return ret;
+}
+
 struct EntryData* get_data()
 {
   struct EntryData* data = malloc(STRINT_MAX_COUNT * sizeof(struct EntryData));
 
+  size_t str_begin = 0;
+  size_t str_end = 0;
+  size_t str_count = 0;
+
+  const gchar* status_data = get_pantry_status();
+
   for (size_t i = 0; i < STRINT_MAX_COUNT; ++i) {
+    ++str_count;
     data[i].text = calloc(255, sizeof(char));
+
+    while (status_data[str_end] && status_data[str_end] != ',') {
+      ++str_end;
+    }
+
+    memcpy(data[i].text, &status_data[str_begin], (str_end - str_begin - 2) * sizeof(char));
+
+    data[i].status = status_data[str_end - 1] - '0';
+
+    if (status_data[str_end] == 0) {
+      data[i+1].text = calloc(255, sizeof(char));
+      break;
+    }
+
+    str_end = str_end + 1;
+    str_begin = str_end;
   }
 
-  strcpy(data[0].text, "hello");
-  strcpy(data[1].text, "world");
-  strcpy(data[2].text, "more");
-  strcpy(data[3].text, "buttons");
-  strcpy(data[3].text, "rossi long strings big sentence even");
+  free(status_data);
+
   return data;
 }
 
